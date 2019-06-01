32b052db5490e6c8b1847794fd8c7d9493ffb836
Scott C. Livingston <scott@rerobots.net>
2019-06-01 16:47:34 -0700

    DOC

--- mistygrind/__init__.py
+++ mistygrind/__init__.py
@@ -1,3 +1,7 @@
+"""a tool for static analysis of Misty skills and offboard Misty REST API clients
+
+The repository is at https://github.com/rerobots/mistygrind
+"""
 try:
     from ._version import __version__
 except ImportError:
