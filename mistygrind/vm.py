77dd45a951fd5d55d0c2a872012c853ee65b7389
Scott C. Livingston <scott@rerobots.net>
2020-02-06 12:25:28 -0800

    GET /pubsub, system/updates, {audio,images}/list

--- mistygrind/vm.py
+++ mistygrind/vm.py
@@ -2,6 +2,7 @@
 SCL <scott@rerobots.net>
 Copyright (c) 2020 rerobots, Inc.
 """
+import aiohttp
 from aiohttp import web
 
 
@@ -69,6 +70,62 @@ async def device(request):
     })
 
 
+async def get_system_updates(request):
+    """GET /api/system/updates
+
+    cf. https://docs.mistyrobotics.com/misty-ii/rest-api/api-reference/#getstoreupdateavailable
+    """
+    return web.json_response({
+        'status': 'Success',
+        'result': False,
+    })
+
+
+async def get_audio_list(request):
+    """GET /api/audio/list
+
+    cf. https://docs.mistyrobotics.com/misty-ii/rest-api/api-reference/#getaudiolist
+    """
+    return web.json_response({
+        'status': 'Success',
+        'result': [
+            {
+                'name': 'fake.wav',
+                'systemAsset': True,
+            },
+        ],
+    })
+
+
+async def get_images_list(request):
+    """GET /api/images/list
+
+    cf. https://docs.mistyrobotics.com/misty-ii/rest-api/api-reference/#getimagelist
+    """
+    return web.json_response({
+        'status': 'Success',
+        'result': [
+            {
+                'name': 'fake.jpg',
+                'systemAsset': True,
+                'height': 272,
+                'width': 400,
+            },
+        ],
+    })
+
+
+async def pubsub(request):
+    """WebSocket via /pubsub
+    """
+    ws = web.WebSocketResponse()
+    await ws.prepare(request)
+    async for msg in ws:
+        if msg.type == aiohttp.WSMsgType.CLOSED or msg.type == aiohttp.WSMsgType.ERROR:
+            break
+    return ws
+
+
 async def default_route(request):
     print('{} {}'.format(request.method, request.path))
     return web.json_response(status=404)
@@ -78,5 +135,9 @@ def start_vm():
     app = web.Application(middlewares=[cors_handler])
     app.router.add_get(r'/api/device', device)
     app.router.add_get(r'/api/battery', battery)
+    app.router.add_get(r'/api/system/updates', get_system_updates)
+    app.router.add_get(r'/api/audio/list', get_audio_list)
+    app.router.add_get(r'/api/images/list', get_images_list)
+    app.router.add_get(r'/pubsub', pubsub)
     web.run_app(app, host='127.0.0.1', port=8888)
     return 0
